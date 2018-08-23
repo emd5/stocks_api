81cf50bf4740682afcd70e75f9add9506e234ef5
emd5 <emahoney5@mail.greenriver.edu>
2018-08-22 22:46:55 -0700

    config routes, remove templates, default, notfound

--- stocks_api/__init__.py
+++ stocks_api/__init__.py
@@ -2,11 +2,12 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """ This function returns a Pyramid WSGI application. Entry point of the application
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.include('.models')
-    config.include('.routes')
-    config.scan()
-    return config.make_wsgi_app()
+    config.include('pyramid_restful')
+    # config.include('.models')
+    config.include('.routes') #relative path to
+
+    config.scan() # looks at @view_config
+    return config.make_wsgi_app() #this is http server web server gateway interface
